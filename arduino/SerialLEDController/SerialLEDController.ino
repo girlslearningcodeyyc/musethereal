36c6ad3ca5513be0efb2fa32a8c396193910761e
Kent Brockman <KBrockman@matrix-solutions.com>
2016-03-15 20:56:12 -0600

    Simplify, boost colors, remove extra LED array

--- arduino/SerialLEDController/SerialLEDController.ino
+++ arduino/SerialLEDController/SerialLEDController.ino
@@ -3,30 +3,24 @@
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Set up first array of LEDs
-#define ARRAY1PIXELS      4
+#define ARRAY1PIXELS      20
 #define ARRAY1PIN         6
 Adafruit_NeoPixel array1 = Adafruit_NeoPixel(ARRAY1PIXELS, ARRAY1PIN, NEO_GRB + NEO_KHZ800);
 
-// Set up second array of LEDs
-#define ARRAY2PIXELS      22
-#define ARRAY2PIN         10
-Adafruit_NeoPixel array2 = Adafruit_NeoPixel(ARRAY2PIXELS, ARRAY2PIN, NEO_GRB + NEO_KHZ800);
-
-#define COLORSLENGTH  22
+#define COLORSLENGTH  20
 char colors [COLORSLENGTH];
 
 int delayval = 50; // delay for half a second
 
-void setup() {    
+void setup() {
   Serial.begin(9600);
-  array1.begin(); 
-  array2.begin();
+  array1.begin();
 
-  for(int i=0;i<COLORSLENGTH;i++){
+  for (int i = 0; i < COLORSLENGTH; i++) {
     colors[i] = 'n';
   }
 
@@ -35,56 +29,53 @@ void setup() {
 
 void loop() {
   char c;
-  
-  if(Serial.available()) {  
-    c = Serial.read();  
 
-    if (c != NULL) {
+  if (Serial.available()) {
+    c = Serial.read();
+
+    if (c != 0) {
       addToColors(c);
       writeColorsToLEDs();
     }
-  } 
+  }
 }
 
-void addToColors(char c){
-  for (int i=COLORSLENGTH-1;i>0;i--) {
-    colors[i] = colors[i-1];
+void addToColors(char c) {
+  for (int i = COLORSLENGTH - 1; i > 0; i--) {
+    colors[i] = colors[i - 1];
   }
 
   colors[0] = c;
 }
 
-void writeColorsToLEDs(){
-  for(int i=0;i<ARRAY1PIXELS;i++) {
+void writeColorsToLEDs() {
+  for (int i = 0; i < ARRAY1PIXELS; i++) {
     array1.setPixelColor(i, convertToColor(colors[i])); // Moderately bright green color.
   }
 
-  for(int i=0;i<ARRAY2PIXELS;i++) {
-    array2.setPixelColor(i, convertToColor(colors[i])); // Moderately bright green color.
-  }
-
-  array1.show(); 
-  array2.show(); 
+  array1.show();
 }
 
 uint32_t convertToColor(char c) {
-  switch(c){
+  switch (c) {
     case 'r':
       return 0xFF0000;
     case 'o':
-      return 0x992200;
+      return 0xFF8800;
     case 'y':
-      return 0x999900;
+      return 0xFFFF00;
     case 'g':
-      return 0x009900;
+      return 0x00FF00;
     case 'c':
-      return 0x009999;  
+      return 0x00FFFF;
     case 'b':
-      return 0x000099;
+      return 0x0000FF;
     case 'v':
-      return 0x660099;
+      return 0xAA00FF;
     case 'p':
-      return 0x990099;  
+      return 0xFF00FF;
+    case 'w':
+      return 0xFFFFFF;
     default:
       return 0x000000;
   }
