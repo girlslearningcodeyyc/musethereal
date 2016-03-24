3538fb0d11a11bbe190f639880760034f3a5e92f
Kent Brockman <KBrockman@matrix-solutions.com>
2016-03-23 20:14:06 -0600

    Move some files around, put in example code

--- android/example/src/main/java/com/emotiv/EEGHeadset.java
+++ android/example/src/main/java/com/emotiv/EEGHeadset.java
@@ -10,4 +10,8 @@ public class EEGHeadset {
     public EEGHeadset() {
 
     }
+
+    public void Connect(){
+        //Get the headset
+    }
 }
--- android/example/src/main/java/com/felhr/serialportexample/DressController.java
+++ android/example/src/main/java/com/felhr/serialportexample/DressController.java
@@ -4,4 +4,7 @@ package com.felhr.serialportexample;
  * Created by kbrockman on 16-03-23.
  */
 public class DressController {
+    public DressController() {
+
+    }
 }
